5aea5d44f504fd73451a7de65d710ed06cc35e4a
samittz1718-cloud <samittz1718@gmail.com>
2025-10-16 22:01:17 -0500

    Agregadas apps accounts y messages_app + actualizados settings y urls

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clientes/', include('clientes.urls')),  # conecta tu app
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),                 # Home/About/Pages
+    path("accounts/", include("accounts.urls")),    # Auth y perfiles
+    path("messages/", include("messages_app.urls")),# Mensajería
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
